cabb58454c378c9842fefb0ca2136d0fe24fbc84
javiair <javiair001@gmail.com>
2019-05-30 01:21:43 -0400

    Fixed routes on routing module.

--- Front_End_Angular/movies-for-fun/src/app/app-routing.module.ts
+++ Front_End_Angular/movies-for-fun/src/app/app-routing.module.ts
@@ -4,16 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { MovieDetailComponent } from './home/movie-detail/movie-detail.component';
 import { MovieListComponent } from './home/movie-list/movie-list.component';
 import { MovieItemComponent } from './home/movie-list/movie-item/movie-item.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { MovieEditComponent } from './home/movie-edit/movie-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
   { path: 'movies', component: HomeComponent},
-  { path: 'movie/detail', component: MovieDetailComponent}
+  { path: 'movies/:title', component: MovieDetailComponent},
+  { path: 'movies/:title/edit', component: MovieEditComponent},
+  { path: 'addMovie', component: MovieEditComponent}
+
+
 
   // this is for if i want home to have two child routes or swithc between movie-list and movie-detail
   // { path: '', component: HomeComponent, children: [
   //   {path: 'movies', component: MovieListComponent},
-  //   {path: 'movie/detail', component: MovieDetailComponent}
+  //   {path: ':title', component: MovieDetailComponent}
   // ]}
 ];
 
