fc34aec70cf2cb445f66fa4d761e8b4cc5e81a83
javiair <javiair001@gmail.com>
2019-05-30 01:32:31 -0400

    Updated movie model to fit all requirements.

--- Front_End_Angular/movies-for-fun/src/app/models/movie.model.ts
+++ Front_End_Angular/movies-for-fun/src/app/models/movie.model.ts
@@ -1,5 +1,10 @@
 export class Movie {
-
-  constructor( public title: string, public description: string) {}
-
+  constructor(
+    public _id: String,
+    public title: String,
+    public description: String,
+    public image: String,
+    public genre: String,
+    public rating: Number
+  ) {}
 }
