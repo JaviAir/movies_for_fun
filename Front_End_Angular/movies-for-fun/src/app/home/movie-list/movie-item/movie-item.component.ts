81e9274bc506d88c5201417e044dbf9598af5ac5
javiair <javiair001@gmail.com>
2019-06-06 01:27:49 -0400

    Saves index of selected movie & sends title to url

--- Front_End_Angular/movies-for-fun/src/app/home/movie-list/movie-item/movie-item.component.ts
+++ Front_End_Angular/movies-for-fun/src/app/home/movie-list/movie-item/movie-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Movie } from 'src/app/models/movie.model';
-import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-item',
@@ -9,18 +9,30 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent implements OnInit {
+  @Input() movie: Movie;
+  @Input() index: number;
+  searchBarTitle: String;
 
-   @Input() movie: Movie;
+  constructor(private movieService: MoviesService, private route: Router) {}
 
-  constructor(private movieService: MoviesService) { }
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   savePageYOffset() {
     // Saves the previous pages' (movielist.component) Y offset and stores it into in-memory pageYOffset
     this.movieService.pageYOffset = window.pageYOffset;
+    this.convertSpaceToUnderscore(this.movie.title);
+    this.route.navigate(['/movies', this.searchBarTitle]);
+
+    this.movieService.currentMovieIndex = this.index; // saves index of currently selected movieItem in-memory
   }
 
+  convertSpaceToUnderscore(title: String) {
+    for (const char in title) {
+      if (title[char] === ' ') {
+        title = title.replace(' ', '_');
+      }
+    }
+    console.log(title.toLowerCase());
+    this.searchBarTitle = title.toLowerCase();
+  }
 }
