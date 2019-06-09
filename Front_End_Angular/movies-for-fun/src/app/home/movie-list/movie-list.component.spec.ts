454624c02ab35a1ccacd034a617ff6fc50e95259
javiair <javiair001@gmail.com>
2019-06-09 19:13:48 -0400

    Added proper imports and declarations.

--- Front_End_Angular/movies-for-fun/src/app/home/movie-list/movie-list.component.spec.ts
+++ Front_End_Angular/movies-for-fun/src/app/home/movie-list/movie-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieListComponent } from './movie-list.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MovieListComponent', () => {
   let component: MovieListComponent;
@@ -8,7 +11,10 @@ describe('MovieListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovieListComponent ]
+      imports: [
+        RouterTestingModule, HttpClientTestingModule
+      ],
+      declarations: [ MovieListComponent, MovieItemComponent ]
     })
     .compileComponents();
   }));
