5d058307f881809c99b81b61ef1e59ce86f41508
javiair <javiair001@gmail.com>
2019-06-09 19:16:35 -0400

    Added imports and test cases for user buttons.

--- Front_End_Angular/movies-for-fun/src/app/home/movie-detail/movie-detail.component.spec.ts
+++ Front_End_Angular/movies-for-fun/src/app/home/movie-detail/movie-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieDetailComponent } from './movie-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MovieDetailComponent', () => {
   let component: MovieDetailComponent;
@@ -8,6 +10,7 @@ describe('MovieDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [ MovieDetailComponent ]
     })
     .compileComponents();
@@ -22,4 +25,19 @@ describe('MovieDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should edit movie from edit button', () => {
+    fixture = TestBed.createComponent(MovieDetailComponent);
+    component = fixture.componentInstance;
+    component.onEdit();
+    expect(component.editMode).toBe(true);
+  });
+
+  it('should delete movie from delete button', () => {
+    fixture = TestBed.createComponent(MovieDetailComponent);
+    component = fixture.componentInstance;
+    component.onDelete();
+    // make it expect something with spyon or toHaveBeenCalled()
+  });
+
 });
