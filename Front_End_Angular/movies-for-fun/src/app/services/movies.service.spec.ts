700ef58882968010176c2111f89910d249429b2e
javiair <javiair001@gmail.com>
2019-06-09 19:11:16 -0400

    Added proper imports and providers.

--- Front_End_Angular/movies-for-fun/src/app/services/movies.service.spec.ts
+++ Front_End_Angular/movies-for-fun/src/app/services/movies.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [MoviesService]
+  }));
 
   it('should be created', () => {
     const service: MoviesService = TestBed.get(MoviesService);
