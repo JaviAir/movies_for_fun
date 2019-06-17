eb989ecfa46776e6b44d9e7fbd693cebbf72333f
javiair <javiair001@gmail.com>
2019-06-16 21:49:53 -0400

    Created spec file for future testing.

---
+++ Front_End_Angular/movies-for-fun/src/app/models/movie.model.spec.ts
@@ -0,0 +1,13 @@
+// import { TestBed } from '@angular/core/testing';
+// import { Movie } from './movie.model';
+
+// describe('MovieModel', () => {
+//   beforeEach(() => TestBed.configureTestingModule({
+//     providers: [Movie]
+//   }));
+
+//   it('should be created', () => {
+//     const model: Movie = TestBed.get(Movie);
+//     expect(model).toBeTruthy();
+//   });
+// });
