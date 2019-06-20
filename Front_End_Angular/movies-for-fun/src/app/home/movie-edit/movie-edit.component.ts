4f3e2eb3ee87778b665466dabd1e0fa312b0470f
javiair <javiair001@gmail.com>
2019-06-19 23:38:14 -0400

    Component now handles page refresh/ new instances.

--- Front_End_Angular/movies-for-fun/src/app/home/movie-edit/movie-edit.component.ts
+++ Front_End_Angular/movies-for-fun/src/app/home/movie-edit/movie-edit.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Location } from '@angular/common';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movie-edit',
   templateUrl: './movie-edit.component.html',
   styleUrls: ['./movie-edit.component.css']
 })
-export class MovieEditComponent implements OnInit {
+export class MovieEditComponent implements OnInit, OnDestroy {
   title: String = '';
   editMode = false;
   movieForm: FormGroup;
+  movie: Movie;
+  showSpinner = true;
+
+  private movieSubjectSubscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,13 +30,31 @@ export class MovieEditComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+        this.movieSubjectSubscription = this.movieService.movieSubject.subscribe(
+      (movie: Movie) => {
+          this.movie = movie;
+        if (this.movie) {
+          this.initForm();
+        }
+        if (movie === undefined) {
+          this.movieService.getMovieInstance(this.title);
+        } else {
+          this.showSpinner = false;
+        }
+      }
+    );
     this.route.queryParams.subscribe((queryParams: Params) => {
       this.editMode = queryParams['edit'];
     });
     this.route.params.subscribe((params: Params) => {
       this.title = params['title'];
-      this.initForm();
+
     });
+    if (this.title === undefined && this.editMode === undefined) {
+      this.initForm();
+    } else {
+          this.movieService.getMovie(this.title);
+    }
   }
 
   onSubmit(content) {
@@ -39,8 +63,6 @@ export class MovieEditComponent implements OnInit {
     if (this.editMode) {
       this.movieService.updateMovie(this.title, this.movieForm.value);
       this.modalService.open(content, {centered: true, size: 'lg'});
-      // this._location.back();
-      // this.router.navigate(['/movies', this.title]);
     } else {
       this.movieService.addMovietoDatabase(this.movieForm.value);
       this.router.navigate(['/movies']);
@@ -49,6 +71,10 @@ export class MovieEditComponent implements OnInit {
 
   onCancel() {
       this._location.back();
+  }
+
+  ngOnDestroy() {
+    this.movieSubjectSubscription.unsubscribe();
   }
 
   // method where
@@ -64,12 +90,11 @@ export class MovieEditComponent implements OnInit {
     let movieRating: String = '';
 
     if (this.editMode) {
-      const movie = this.movieService.getMovie(this.title);
-      movieTitle = movie.title;
-      movieDescription = movie.description;
-      movieImagePath = movie.image;
-      movieGenre = movie.genre;
-      movieRating = movie.rating;
+      movieTitle = this.movie.title;
+      movieDescription = this.movie.description;
+      movieImagePath = this.movie.image;
+      movieGenre = this.movie.genre;
+      movieRating = this.movie.rating;
 
     }
 
