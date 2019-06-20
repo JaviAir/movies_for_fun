21f62eb8e6d954bb09c039f1465b344b18ab3b55
javiair <javiair001@gmail.com>
2019-06-19 23:46:32 -0400

    Updated endpoints.

--- Front_End_Angular/movies-for-fun/src/app/services/http.service.ts
+++ Front_End_Angular/movies-for-fun/src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Movie } from '../models/movie.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,46 +10,52 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   // CRUD
+
+  apiUrl = 'http://localhost:4000/';
   // local = 'http://localhost:4000/';
   // firebase was firebaseprofile/app
-  herokuUrl = 'https://stark-brook-68539.herokuapp.com/';
+  //
 
   getMovies(n: Number) {
-    return this.http.post<Movie[]>( this.herokuUrl + 'pagemovies', {
+    return this.http.post<Movie[]>( this.apiUrl + 'pagemovies', {
       n: n
     });
   }
 
+  getMovieInstance(urlTitle: String) {
+    return this.http.get<Movie>( this.apiUrl + 'movies/' + urlTitle);
+  }
+
   addMovie(movie: Movie) {
-    return this.http.post(this.herokuUrl + 'movies', movie);
+    return this.http.post(this.apiUrl + 'movies', movie);
   }
 
   updateMovie(oldTitle: String, movie: Movie) {
-    return this.http.put(this.herokuUrl + 'movies/' + oldTitle, movie);
+    return this.http.put(this.apiUrl + 'movies/' + oldTitle, movie);
   }
 
   deleteMovie(oldTitle: String) {
-    return this.http.delete(this.herokuUrl + 'movies/' + oldTitle);
+    return this.http.delete(this.apiUrl + 'movies/' + oldTitle);
   }
 
   // FILTERS
 
   getAlphabetizedOrder(char: String, skipNumber: Number) {
-    return this.http.post(this.herokuUrl + 'movies' + '/filterchar', {
+    return this.http.post(this.apiUrl + 'movies' + '/filterchar', {
       char: char,
       n: skipNumber
     });
   }
 
   filterGenre(genre: String, skipNumber: Number) {
-    return this.http.post(this.herokuUrl + 'movies' + '/filtergenre', {
+    return this.http.post(this.apiUrl + 'movies' + '/filtergenre', {
       genre: genre,
       n: skipNumber
     });
   }
 
   filterBoth(char: String, genre: String, skipNumber: Number) {
-    return this.http.post(this.herokuUrl + 'movies' + '/filterboth', {
+    return this.http.post(this.apiUrl + 'movies' + '/filterboth', {
       char: char,
       genre: genre,
       n: skipNumber
