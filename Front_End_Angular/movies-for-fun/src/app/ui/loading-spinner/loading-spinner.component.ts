dddac276ae4fab6df6a21a489d7c0338c50c2ab6
javiair <javiair001@gmail.com>
2019-06-21 03:47:15 -0400

    Testing Spinner.

--- Front_End_Angular/movies-for-fun/src/app/ui/loading-spinner/loading-spinner.component.ts
+++ Front_End_Angular/movies-for-fun/src/app/ui/loading-spinner/loading-spinner.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.css']
 })
-export class LoadingSpinnerComponent implements OnInit {
+export class LoadingSpinnerComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+    console.log('spinner created');
+  }
+
+  ngOnDestroy() {
+    console.log('spinner destroyed');
   }
 
 }
