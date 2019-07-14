444712cc47ce9320cde9332c1cedbae58c77052a
javiair <javiair001@gmail.com>
2019-07-13 23:54:10 -0400

    Implemented resolver for MovieDetail Component.

--- Front_End_Angular/movies-for-fun/src/app/app-routing.module.ts
+++ Front_End_Angular/movies-for-fun/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MovieDetailComponent } from './home/movie-detail/movie-detail.component';
 import { MovieEditComponent } from './home/movie-edit/movie-edit.component';
+import { MovieDetailResolver } from './resolvers/movie-detail.resolver';
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
   { path: 'movies', component: HomeComponent},
-  { path: 'movies/:title', component: MovieDetailComponent},
+  { path: 'movies/:title', component: MovieDetailComponent, resolve: {movieInfo: MovieDetailResolver}},
   { path: 'movies/:title/edit', component: MovieEditComponent},
   { path: 'addMovie', component: MovieEditComponent},
   { path: '**', redirectTo: '/movies'}
@@ -15,6 +16,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'disabled'}) ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [MovieDetailResolver]
 })
 export class AppRoutingModule { }
